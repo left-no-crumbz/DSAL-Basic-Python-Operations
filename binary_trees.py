7f86fbd3e08ba39dffab068ff674df2b32532e35
Dzone <133969306+left-no-crumbz@users.noreply.github.com>
2023-09-22 22:34:46 +0800

    KERVIN TAKE D WHEEL !

--- binary_trees.py
+++ binary_trees.py
@@ -13,36 +13,72 @@ class Node:
         # else: self.data = data
 
             if data < self.data:             
-                if self.left is None: self.left = Node(data)
-                else: self.left.insert(data)
+                if self.left is None: 
+                    self.left = Node(data)
+                else: 
+                    self.left.insert(data)
             elif data > self.data:
-                if self.right is None: self.right = Node(data)
-                else: self.right.insert(data)
-        else: self.data = data
+                if self.right is None: 
+                    self.right = Node(data)
+                else: 
+                    self.right.insert(data)
+        else: 
+            self.data = data
 
     def inorder_traversal(self, root):
         res = []
         if root:
-            res = self.inorder_traversal(root.left)
+            res += self.inorder_traversal(root.left)
             res.append(root.data)
-            res = res + self.inorder_traversal(root.right)
+            res += self.inorder_traversal(root.right)
         return res
 
-    def preorder_traversal(self, root): #TODO: this doesnt work rn T_T
+    def preorder_traversal(self, root):
         res = []
         if root:
             res.append(root.data)
-            res = self.preorder_traversal(root.left)
-            res = res + self.preorder_traversal(root.right)
+            res += self.preorder_traversal(root.left)
+            res += self.preorder_traversal(root.right)
         return res
 
     def postorder_traversal(self, root):
         res = []
         if root:
-            res = self.postorder_traversal(root.left)
-            res = res + self.postorder_traversal(root.right)
+            res += self.postorder_traversal(root.left)
+            res += self.postorder_traversal(root.right)
             res.append(root.data)
         return res
+
+
+# menu
+
+print("1. Assign value to root node")
+print("2. Insert value into tree")
+print("3. Inorder traversal")
+print("4. Preorder traversal")
+print("5. Postorder traversal")
+print("6. Exit")
+
+choice = int(input("Please enter a number: "))
+root = ""
+match(choice):
+    case 1:
+        root = Node(ord(input("Enter a character as the root node: ")))
+    case 2:
+        branch = input("Enter a character you want to insert")
+        root.insert(branch)
+    case 3:
+        print(root.inorder_traversal(root))
+    case 4:
+        print(root.preorder_traversal(root))
+    case 5:
+        print(root.postorder_traversal(root))
+    case 6:
+        exit()
+
+
+
+
 
 root = Node(10)
 root.insert(5)
@@ -51,8 +87,6 @@ root.insert(8)
 root.insert(14)
 root.insert(12)
 root.insert(16)
-
-
 
 
 print(root.inorder_traversal(root))
